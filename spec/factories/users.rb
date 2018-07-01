ae68e087c5a75d968b0feb56a036441cb329f19b
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-07-01 13:47:37 +0300

    Adding a user registration function

---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :simple_user, class: User do
+    email                 "simple_user@mail.no"
+    password              "123456"
+  end
+end
---
+++ spec/features/user_registration_spec.rb
@@ -0,0 +1,44 @@
+require "rails_helper"
+
+feature 'User registration', type: :feature do
+  before do
+    visit root_path
+  end
+
+  context ' When user do not registered' do
+    scenario 'User can signup' do
+      click_link 'Log In'
+      click_link 'Sign up'
+      fill_in 'Email', with: build(:simple_user).email
+      fill_in 'Password', with: build(:simple_user).password
+      fill_in 'Password confirmation', with: build(:simple_user).password
+      click_button 'Sign up'
+      expect(page).to have_content "Current user: #{build(:simple_user).email}"
+    end
+
+    scenario 'User can login' do
+      @user=create(:simple_user)
+      click_link 'Log In'
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+      click_button 'Log in'
+      expect(page).to have_content "Current user: #{@user.email}"
+    end
+  end
+
+  context ' When user is registered' do
+    before do
+      @user=create(:simple_user)
+      click_link 'Log In'
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+      click_button 'Log in'
+    end
+
+    scenario 'User can logout' do
+      expect(page).to have_content "Current user: #{@user.email}"
+      click_link 'Log Out'
+      expect(page).to_not have_content "Current user: #{@user.email}"
+    end
+  end
+end
