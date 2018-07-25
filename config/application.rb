a8121d258c4204f409fdaf1e3f540f827e2154f4
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-07-25 22:50:40 +0300

    Handled errors in fields by Bootstrap

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,15 @@ module FAODatabase
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
+      class_attr_index = html_tag.index 'class="'
+
+      if class_attr_index
+        html_tag.insert class_attr_index + 7, 'is-invalid '
+      else
+        html_tag.insert html_tag.index('>'), ' class="is-invalid"'
+      end   
+    }
   end
 end
