e3c9c6e0613427e5dd9a84f9fdd3438ca87a4fd9
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-07-30 20:07:22 +0300

    Root changed

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :questionnaires
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'single_pages#under_construction' 
+  # root 'single_pages#under_construction'
+  root  'questionnaires#index'
 end
