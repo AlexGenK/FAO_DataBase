f2fc8b80154b66726b1a7ec5da74818911a93b42
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-07-30 22:26:51 +0300

    Aded context 'when User is not logged in' to questionnaires spec

--- spec/features/questionnaires_spec.rb
+++ spec/features/questionnaires_spec.rb
@@ -62,4 +62,35 @@ feature 'Work with questionnaires', type: :feature do
       expect(page).to have_questionnaires_in_quantity 5
     end
   end
+
+  context 'when User is not logged in' do
+    scenario 'User can view shorted index of questionnaires' do
+      expect(page).to have_questionnaires_in_quantity 5
+      expect(page).to have_content('???????', count: 5)
+    end
+
+    scenario 'User can not add questionnaire' do
+      click_link 'Создать новую анкету'
+      expect(page).to have_content 'You need to sign in or sign up before continuing.'
+    end
+
+    scenario 'User can not delete questionnaire' do
+      click_on(class: 'btn-outline-danger', match: :first)
+      expect(page).to have_content 'You need to sign in or sign up before continuing.'
+    end
+
+    scenario 'User can not edit questionnaire' do
+      click_on('???????', match: :first)
+      expect(page).to have_content 'You need to sign in or sign up before continuing.'
+    end
+
+    scenario 'User can find questionnaire by code' do
+      fill_in('search', with: @quest_list[2].code)
+      click_button 'Найти'
+      expect(page).to have_questionnaires_in_quantity 1
+      expect(page).to have_content(@quest_list[2].code, count: 1)
+      click_button 'Показать все'
+      expect(page).to have_questionnaires_in_quantity 5
+    end
+  end
 end
